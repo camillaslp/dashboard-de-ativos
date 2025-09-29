10c0e327e35cc4f2e013582da97d48e130074be3
camillaslp <90100020+camillaslp@users.noreply.github.com>
2025-09-29 00:57:16 +0000

    atualização de código para utilização de Google Sheet para conseguir salva alteração a cada sessão

--- main.py
+++ main.py
@@ -3,8 +3,15 @@ import yfinance as yf
 import json
 import os
 import warnings
-
+from google.oauth2.service_account import Credentials
+import gspread
 warnings.filterwarnings("ignore", category=FutureWarning)
+
+creds_dict = json.loads(st.secrets["GOOGLE_CREDS"])
+scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/drive']
+creds = Credentials.from_service_account_info(creds_dict, scopes=scope)
+client = gspread.authorize(creds)
+sheet = client.open("Acoes").sheet1
 
 # --------------- Config ----------------
 ARQUIVO_ACOES = "acoes.json"
